5ccd8a05c99581cc7c99f6806fb4ad69b9a2c4f3
aftoncameron <acameron@gmail.com>
2018-05-30 16:22:12 -0500

    removes layout

--- addon/components/g-table.js
+++ addon/components/g-table.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import layout from '../templates/components/g-table';
+// import layout from '../templates/components/g-table';
 import {
   computed
 } from '@ember/object';

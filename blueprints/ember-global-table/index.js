480ff141daafd91d32740ee0f0128e264f563fde
aftoncameron <acameron@gmail.com>
2018-05-31 14:58:18 -0500

    testing another approach to dependences

--- blueprints/ember-global-table/index.js
+++ blueprints/ember-global-table/index.js
@@ -1,27 +1,9 @@
-// 'use strict';
-
 module.exports = {
-  normalizeEntityName: function() {
-    // this prevents an error when the entityName is
-    // not specified (since that doesn't actually matter
-    // to us
-  },
+  description: 'dependencies for ember global table',
 
-  afterInstall: function() {
-    return this.addPackagesToProject({
-      packages: [{
-          name: 'ember-truth-helpers'
-        },
-        {
-          name: 'ember-cli-string-helpers'
-        },
-        {
-          name: 'ember-responsive'
-        }
-      ],
-      blueprintOptions: {
-        saveDev: true
-      }
-    });
+  normalizeEntityName: function() {},
+
+  beforeInstall: function() {
+    return this.addAddonToProject('ember-responsive');
   }
 };
